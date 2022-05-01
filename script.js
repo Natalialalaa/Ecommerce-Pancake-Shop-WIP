cae117dea4f72e4ac7c8eebbc1dc190898f4ec3e
Natalialalaa <priscillanatalia.id@gmail.com>
2022-05-01 18:23:04 +0200

    add JS func -radio + fix style and layout

--- script.js
+++ script.js
@@ -18,9 +18,7 @@ window.addEventListener("click", function(e) {
 
 
 
-/*main article 1: pancake image ->
- 3 steps sizing, SLIDER value -> 
- tiny, normal, XxL */
+/*main article 1: pancake image -> 3 steps sizing, SLIDER value -> tiny, normal, XxL */
 
 const slider = document.getElementById("myRange");
 const output = document.getElementById("slide-value");
@@ -32,23 +30,32 @@ slider.oninput = sizingPancake = () => {
   pancakeSize.style.width = (125 * sliderValue) + 'px';
   output.innerHTML = sliderValue < 2 ? 'petit' : sliderValue > 2 ?'XXL' : 'medium' ;
 }
-
 /*abort previous prototype 
-
   slider.oninput = function() {
   output.innerHTML = this.value;
-
   if (this.value = 3){
-    
   } else if (this.value = 9){
-
   }
-
 }*/
 
 
+/*main article 2: flavors image -> 3 images changing -> Radio button value / checked */
 
-/*main article 2: flavors image ->
- 3 images changing -> RADIO BUTTON value -> 
- matcha, chocolate, strawberry */
+const changingToppings = () => {
+  if (document.getElementById("matcha").checked) {
+  document.getElementById("flavours-pic").src = "Visuals/flavour-matcha.png";
+  } else if (document.getElementById("chocolate").checked) { 
+  document.getElementById("flavours-pic").src = "Visuals/flavour-chocolate.png";
+  }  else if (document.getElementById("strawberry").checked) { 
+    document.getElementById("flavours-pic").src = "Visuals/flavour-strawberry.png";
+  }
+}
 
+
+/*main article 3: toppings category -> filter */
+  /* Add the "show" class (display:block) to the filtered elements,
+  and remove the "show" class from the elements that are not selected */
+
+// Show filtered elements
+// Hide elements that are not selected
+// Add active class to the current control button (highlight it)
