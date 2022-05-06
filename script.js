a09ddc53e37171025411cd1869c806e8ef75623d
Natalialalaa <priscillanatalia.id@gmail.com>
2022-05-06 15:26:51 +0200

    JS sidebar + CSS layout fix

--- script.js
+++ script.js
@@ -3,19 +3,18 @@
 */
 
 const login = document.querySelector(".link-login");
-const loginpop = document.querySelector(".dropdown-content")
+const loginpop = document.querySelector(".dropdown-content");
 
 login.addEventListener("mouseover", function() {
   loginpop.style.display = "block";
 });
 
 window.addEventListener("click", function(e) {
-  /*console.log(e);*/
+  //console.log(e);
   if (!loginpop.contains(e.target)) {
     loginpop.style.display = 'none';
   }
 });
-
 
 
 /*main article 1: pancake image -> 3 steps sizing, SLIDER value -> tiny, normal, XxL */
@@ -30,13 +29,6 @@ slider.oninput = sizingPancake = () => {
   pancakeSize.style.width = (125 * sliderValue) + 'px';
   output.innerHTML = sliderValue < 2 ? 'petit' : sliderValue > 2 ?'XXL' : 'medium' ;
 }
-/*abort previous prototype 
-  slider.oninput = function() {
-  output.innerHTML = this.value;
-  if (this.value = 3){
-  } else if (this.value = 9){
-  }
-}*/
 
 
 /*main article 2: flavors image -> 3 images changing -> Radio button value / checked */
@@ -63,8 +55,8 @@ function filterSelection(e) {
   const flavors = document.querySelectorAll(".topping-variants div"); // select all tooping variants divs
   let filter = e.target.dataset.filter;
 
-  console.log(filter); // grab the value in the event target's data-filter attribute
-  console.log(flavors);
+  //console.log(filter); // grab the value in the event target's data-filter attribute
+  //console.log(flavors);
 
   flavors.forEach(flavors => {
 
@@ -77,4 +69,19 @@ function filterSelection(e) {
 
 // Add active class to the current control button (highlight it)
 
+/* !! Work still in progress !!*/
+/*responsive navbar : when user click hamburger icon,
+  show side navbar from the left to the right (dropdown login included on lower part)
+  then blur all background elements, focusing on sidebar */
 
+const burgerIcon = document.querySelector("#sidebar-icon");
+const sidebar = document.querySelector(".top-nav-links");
+
+burgerIcon.addEventListener("click", showSidebar = (e) => {
+  console.log(e)
+  console.log(sidebar);
+
+  //sidebar.style.display = "block";
+  sidebar.classList.toggle("nav-right-test");
+  //sidebar.classList.toggle("nav-right-test");
+});
